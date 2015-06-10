6e3bfd725dd4b806fcb6d5e2b0ae4c75586f03ea
Hack Reactor Students <accounts@hackreactor.com>
2015-06-10 15:25:06 -0700

    Modify port.

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var app = require('./server-config.js');
 var envConfig = require('./env-config.js');
-
+var port = process.env.PORT || envConfig[process.env.NODE_ENV];
 console.log("Node ENV", process.env.NODE_ENV);
 console.log("Process ENV", process.env);
 
-var cfg = envConfig[process.env.NODE_ENV];
 
-app.listen(cfg.port);
+app.listen(port);
 
-console.log('Server now listening on port ', cfg.port);
+
+console.log('Server now listening on port ', port);
